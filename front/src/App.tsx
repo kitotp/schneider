6e6bf2538ca61c6ceb71c07b4a8b8a1e00caa97b
Андрей Слива <cryptomaniaaccton@gmail.com>
2025-11-15 22:12:35 +0100

    (feat): redesign

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,16 +1,26 @@
 import SecondStep from "./components/SecondStep";
 import FirstStep from "./components/FirstStep";
+import { useState } from "react";
 
 
 function App() {
 
+  const [isUploaded, setIsUploaded] = useState(false);
 
   return (
     <div className="">
       <div className="flex flex-col justify-start items-center bg-black min-h-screen">
-        <FirstStep />
+        <FirstStep setIsUploaded={setIsUploaded} />
 
-        <SecondStep />
+        <div
+          className={`
+          w-full flex justify-center
+          transform transition-all duration-500
+          ${isUploaded ? "opacity-100 translate-y-0" : "opacity-0 translate-y-3 pointer-events-none"}
+        `}
+        >
+          <SecondStep />
+        </div>
 
       </div>
     </div>
--- front/src/components/FirstStep.tsx
+++ front/src/components/FirstStep.tsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
 
-const FirstStep = () => {
+type FirstStepProps = {
+    setIsUploaded: (v: boolean) => void
+}
+
+const FirstStep = ({ setIsUploaded }: { setIsUploaded: (v: boolean) => void }) => {
 
     const [file, setFile] = useState<File | null>(null);
     const [accuracy, setAccuracy] = useState<number>()
@@ -30,6 +34,8 @@ const FirstStep = () => {
 
             const data = await res.json()
             setAccuracy(data.accuracy)
+
+            setIsUploaded(true);
         } catch (err: any) {
             throw new Error("error on handle upload.")
         }
@@ -41,10 +47,14 @@ const FirstStep = () => {
             <h1 className="text-2xl font-semibold text-white">
                 1. Download your normalized csv dataset
             </h1>
-            <input type="file" accept=".csv" onChange={handleFileChange} className="mt-3 border p-3 border-white text-white" />
 
-            <button className="text-md hover:bg-white hover:text-black transform duration-300 hover:scale-105 border border-white text-white p-3 mt-3 cursor-pointer " onClick={handleUpload}>Analyze</button>
-            {accuracy && <p className='text-white'>Accuracy: {accuracy}</p>}
+            <label htmlFor="file-upload" className="mt-3 border px-5 py-3 border-white text-white w-[400px] cursor-pointer  hover:bg-white hover:text-black hover:font-semibold transform hover:scale-105 duration-300 ">
+                Choose CSV File
+            </label>
+            <input id='file-upload' type="file" accept=".csv" onChange={handleFileChange} className="hidden" />
+
+            <button className="text-md hover:bg-white hover:text-black hover:font-semibold transform duration-300 hover:scale-105 border border-white text-white p-3 mt-3 cursor-pointer " onClick={handleUpload}>Analyze</button>
+            {accuracy && <p className='text-white font-semibold mt-2'>Accuracy: {accuracy}</p>}
         </div>
     )
 }
